0b79279f8401334fbe6ec05dd8987a122314ae0a
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-18 20:16:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist (playlist, artistName, songTitle){
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
-var playlist = {Kanye: "Gold Digger"}
+
 function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName]
+  return delete playlist[artistName]
       }
